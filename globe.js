cf4036201aaf3e0a35840de00ee0bdd2e1025659
ziweiliu <ziwei.liu@epfl.ch>
2022-05-05 13:41:43 +0200

    root

--- globe.js
+++ globe.js
@@ -10,9 +10,9 @@ const flagEndpoint = 'https://corona.lmao.ninja/assets/img/flags';
 //const getVal = feat => feat.properties.GDP_MD / (5 * 1e6); //  / Math.max(1e5, feat.properties.POP_EST);
 const getVal = feat => feat.properties.total_trades_2019 / (5*1e12); //  / Math.max(1e5, feat.properties.POP_EST);
 
-//fetch('../dataset/classifications_data/ne_110m_admin_0_countries.geojson').then(res => res.json()).then(countries =>{
-fetch('../dataset/countries.geojson').then(res => res.json()).then(countries =>{
-    fetch('../dataset/geo_export.json').then(x => x.json()).then(exports =>{
+//fetch('./dataset/classifications_data/ne_110m_admin_0_countries.geojson').then(res => res.json()).then(countries =>{
+fetch('./dataset/countries.geojson').then(res => res.json()).then(countries =>{
+    fetch('./dataset/geo_export.json').then(x => x.json()).then(exports =>{
         const myGlobe = Globe()
         (document.getElementById('globeViz'))  
         .globeImageUrl('https://unpkg.com/three-globe@2.24.4/example/img/earth-night.jpg')
