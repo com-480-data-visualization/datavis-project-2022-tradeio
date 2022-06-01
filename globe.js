41331be0a92f1c4cab71b89b6f27bdd766ca0ccb
Ziwei-Liu3 <ziwei.liu@epfl.ch>
2022-06-01 18:22:19 +0200

    play button & slider  fixed and start from 1962 when hitting play

--- globe.js
+++ globe.js
@@ -1,17 +1,19 @@
 const COUNTRY = 'United States';
-//const OPACITY = 0.22;
-const OPACITY = 0.99;
-const OPACITY_POLYGONE = 1;
 const colorScale = d3.scaleSequentialSqrt(d3.interpolateYlOrRd);
 const flagEndpoint = 'https://corona.lmao.ninja/assets/img/flags';
 const base_card = document.getElementById('card_placeholder');
-const country_table = document.getElementById('table_placeholder');
-let interval;
+const countryTable = document.getElementById('countriesTable');
+countryTable.style.visibility='hidden';
+const tableBody = document.getElementById('countriesTableBody');
 
-var selected_year = "2019"
-// GDP per capita (avoiding countries with small pop)
-//const getVal = feat => feat.properties.GDP_MD / (5 * 1e6); //  / Math.max(1e5, feat.properties.POP_EST);
-//const getVal = feat => feat.properties.total_trades[selected_year]  / (5*1e12) ; //  / Math.max(1e5, feat.properties.POP_EST);
+const products = ['all', 'food', 'beverage', 'crude_materials', 'fuels', 'vegetable_oil', 'chemicals', 'material_manufacturers', 'machinery', 'other_manufacturers', 'unspecified']
+var trgts_table = []
+var products_dict = {}
+let interval;
+var polygon_country = {lat:0, lng:0, altitude:2};
+
+var selected_year = "2019";
+var selected_prod = "all";
 const getVal = feat => feat.properties.percentage_total[selected_year] / (8)
 
 var myGlobe;
@@ -22,158 +24,33 @@ var lastClickEvent;
 var current_trades;
 var polygon_dict = {};
 var country_locs;
+var oldProd = '';
+var globeClick = false;
 
+fetch('./dataset/country_coords.json').then(res => res.json()).then(coords =>{country_locs = coords; });
+fetch('./dataset/countries.geojson').then(res => res.json()).then(countries =>{init_globe(countries) });
+load_trade_data(products);
 
-fetch('./dataset/country_coords.json').then(res => res.json()).then(coords =>{country_locs = coords; })
-
-
-//fetch('./dataset/classifications_data/ne_110m_admin_0_countries.geojson').then(res => res.json()).then(countries =>{
-fetch('./dataset/countries.geojson').then(res => res.json()).then(countries =>{
-    fetch('./dataset/trade_data_0.json').then(x => x.json()).then(trades =>{
-        current_trades = trades
-        //console.log(trades);
-        myGlobe = Globe()
-        (document.getElementById('globeViz'))  
-        .globeImageUrl('https://unpkg.com/three-globe@2.24.4/example/img/earth-night.jpg')
-        .backgroundImageUrl('https://unpkg.com/three-globe@2.24.4/example/img/night-sky.png')
-        .pointOfView({ lat: 39.6, lng: -98.5, altitude: 2 }) // aim at continental US centroid
-        .onGlobeClick(reset)
-
-
-        //// Arcs
-        //.arcDashLength(0.5)
-        //.arcDashGap(0.5)
-        //.arcDashInitialGap(() => Math.random())
-        //.arcDashAnimateTime(2000)
-        .arcDashLength(0.1)
-        .arcDashGap(0.1)
-        .arcDashAnimateTime(2000)
-        .onArcHover(hoverArc => {
-            //console.log("here")
-            if(hoverArc== null){
-                myGlobe.arcColor(d => {
-                    return d.original_color;
-            });
-            }else{
-                myGlobe.arcColor(d => {                     
-                var split_color = hoverArc.color.substring(5, hoverArc.color.length-1).split(",");
-                //const op = !hoverArc ? split_color[3] : d === hoverArc ? 0.9 : split_color[3] / 4;
-                const op = hoverArc == d? 1:0.15
-                return `rgba(${split_color[0]}, ${split_color[1]}, ${split_color[2]}, ${op})`;
-            });
-
-            }                               
-            
-        })
-        //.arcColor(d => [`rgba(0, 255, 0, ${OPACITY})`, `rgba(255, 0, 0, ${OPACITY})`])
-        .arcColor('color')
-        .arcsTransitionDuration(0)
-        .onGlobeClick(reset)        
-        .arcStroke("stroke")
-        //airport
-        .lineHoverPrecision(0)
-
-
-        //// Polygon
-        .polygonsData(countries.features.filter(d => d.properties.ISO_A2 !== 'AQ'))
-        .polygonAltitude(0.01)
-        .polygonCapColor(feat => convertRGBToRGBA(colorScale(getVal(feat)), OPACITY_POLYGONE))
-        .polygonCapColor(feat => convertRGBToRGBA(colorScale(getVal(feat)), OPACITY_POLYGONE))
-        .polygonSideColor(() => 'rgba(0, 100, 0, 0.15)')
-        .polygonStrokeColor(() => '#111')
-        .polygonsTransitionDuration(300)        
-        .onPolygonHover(hoverD => {
-            myGlobe.polygonAltitude(d => d === hoverD ? 0.1 : 0.01);
-            changeCountryCard(base_card, hoverD);
-        })
-        .onPolygonClick(radiate_arcs)
-
-
-        //// HexPolygon
-        // .hexPolygonsData(countries.features)
-        // .hexPolygonResolution(3)
-        // .hexPolygonMargin(0.6)
-        // .hexPolygonColor(() => `#${Math.round(Math.random() * Math.pow(2, 24)).toString(16).padStart(6, '0')}`)
-        // .hexPolygonLabel(({ properties: d }) => `
-        //   <b>${d.ADMIN} (${d.ISO_A2})</b> <br />
-        //   Population: <i>${d.GDP_MD}</i>
-        // `)
-        // .hexPolygonAltitude(0.011)       
-        
-        
-        for (const poly of myGlobe.polygonsData()) {
-            polygon_dict[poly.properties.ISO_A2] = poly;
-        } 
-              
-        
-    })
-});
-
-
-
-function onTradeChange(selectObject){    
-    //console.log(lastClickEvent)
-    if(GlobaState){
-        radiate_arcs(lastClickEvent["polygon"], lastClickEvent["event"],0,0)   
-    }else{
-        reset(10,10)  
-    }
-}
-
-function onCounChange(selectObject){    
-    const poly =   polygon_dict[selectObject.value]
-    radiate_arcs(poly , 0,0,0)
-    var coord = country_locs[poly.properties.ISO_A2]
-    myGlobe.pointOfView({ lat: coord[0], lng: coord[1], altitude: 2 },1000)  
-}
-
-function onYearChange(year){
-    selected_year = year.toString();
-    if(GlobaState){
-        radiate_arcs(lastClickEvent["polygon"], lastClickEvent["event"],0,0)   
-    }else{
-        reset(10,10)
-    }
-}
 
 function reset({ lat: endLat, lng: endLng }) {
-    base_card.innerHTML = ``
+    base_card.innerHTML = ''
+    countryTable.style.visibility='hidden'
     myGlobe.arcsData([]);
-    myGlobe.polygonCapColor(feat => convertRGBToRGBA(colorScale(getVal(feat)), OPACITY_POLYGONE))
+    myGlobe.polygonCapColor(feat => colorScale(getVal(feat)))
     GlobaState = false 
     document.getElementById("coun") .value = ""    
     myGlobe.labelsData([])
 
     myGlobe.onPolygonHover(hoverD => {
         myGlobe.polygonAltitude(d => d === hoverD ? 0.1 : 0.01);
-        changeCountryCard(base_card, hoverD);
     })  
 }
 
-function money_amount_fixer(amount){
-    return d3.format('.4s')(amount).replace(/G/,"B USD").replace(/M/,"M USD").replace(/k/,"k USD")
-}  
-
-function PolygonColorChanger(d,polygon,arcArray){
-    var targets_A2 = arcArray.map(x => x[3]);
-    var target_Name = arcArray.map(x => x[4]);
-    if (d === polygon){
-        //console.log(d)
-        //steelblue
-        return `rgba(70, 130, 180, ${OPACITY_POLYGONE})`
-    }else if(targets_A2.includes(d.properties.ISO_A2) || target_Name.includes(d.properties.ADMIN)){
-        //lightsalmon
-        return `rgba(255, 160, 122, ${OPACITY_POLYGONE})`
-    }                    
-    else{
-        //grey
-        return `rgba(128, 128, 128, ${OPACITY_POLYGONE})`
-    }
-}
 
 function radiate_arcs(polygon, event, { lat: clicklat, lng:clicklng, altitude }){
-
-    reset(10,10)
+    polygon_arc = polygon
+    //Reset polygonLabel
+    myGlobe.controls().domElement.previousElementSibling.innerHTML = ''
     //After reset display the country card again
     changeCountryCard(base_card, polygon)
     document.getElementById("coun").value = polygon.properties.ISO_A2;    
@@ -187,10 +64,10 @@ function radiate_arcs(polygon, event, { lat: clicklat, lng:clicklng, altitude })
         changeCountryCard(base_card, polygon);
     })
     
+
     //const arc = { startLat: startlat, startLng: startlng, endLat:39.6, endLng:-98.5 };
     //myGlobe.arcsData([...myGlobe.arcsData(), arc]);
     
-    //Data has already been cleaned, no need to change the ADMIN property for france, norway and Kosovo   
     var tradeType =  document.getElementById("trade").value ;   
     
     var arcArray = current_trades[selected_year][polygon.properties.ISO_A2][tradeType];
@@ -199,15 +76,20 @@ function radiate_arcs(polygon, event, { lat: clicklat, lng:clicklng, altitude })
     var startLngx = 0;
     var endLat = 0;
     var endLng = 0;
+    countriesTable = []
+
     for (var i = 0; i < arcArray.length; i++) {
-        
-        
         var src = country_locs[arcArray[i][1]];
         var trgt = country_locs[arcArray[i][3]];
+
+        countriesTable.push([trgt[2], money_amount_fixer(arcArray[i][0]).replace(' USD', ''), {lat:trgt[0], lng:trgt[1], altitude:2.5}]) //Add name and value to the list of countries for the table
+        
         if(tradeType == "import_value"){
             src = country_locs[arcArray[i][3]];
             trgt = country_locs[arcArray[i][1]];
         }
+
+
         startLat = src[0];
         startLng_ = src[1];
         endLat = trgt[0];
@@ -228,12 +110,21 @@ function radiate_arcs(polygon, event, { lat: clicklat, lng:clicklng, altitude })
             name: `${arcArray[i][1]} &#8594; ${arcArray[i][3]} : ${money_amount_fixer(arcArray[i][0])}`
         }
         //console.log(arcArray[i][9] *255 * 5)
-        allArcs =   [...allArcs, newArc]             
+        allArcs =   [...allArcs, newArc]
+        
     }
+
+    polygon_country.lat = tradeType == "import_value" ? trgt[0] : src[0]
+    polygon_country.lng = tradeType == "import_value" ? trgt[1] : src[1]
+     
+    myGlobe.pointOfView(polygon_country, 1000)
+
+    
+    changeCountryTable(countryTable, tableBody, countriesTable, tradeType)
+
     myGlobe.arcsData(allArcs);    
-    //myGlobe.polygonCapColor(d => d === polygon ? 'steelblue' : "lightsalmon")
     myGlobe.polygonCapColor(d => PolygonColorChanger(d,polygon,arcArray))     
-    //console.log(exports[polygon.properties.ISO_A3]);
+
     var clickLabel = ["a", "a", "a", arcArray[0][1]];
 
     //console.log(country_locs)
@@ -246,7 +137,61 @@ function radiate_arcs(polygon, event, { lat: clicklat, lng:clicklng, altitude })
     myGlobe.labelColor(() => 'darkturquoise')                
     myGlobe.labelResolution(8)
     myGlobe.labelAltitude(0.025)
-
-    //myGlobe.hexPolygonsData(exports[polygon.properties.ISO_A3])
 }       
 
+
+function init_globe(countries){
+    myGlobe = Globe()
+    (document.getElementById('globeViz'))  
+    .globeImageUrl('https://unpkg.com/three-globe@2.24.4/example/img/earth-night.jpg')
+    .backgroundImageUrl('https://unpkg.com/three-globe@2.24.4/example/img/night-sky.png')
+    .pointOfView({ lat: 39.6, lng: -98.5, altitude: 2 }) // aim at continental US centroid
+
+    //// Arcs
+    .arcDashLength(0.1)
+    .arcDashGap(0.1)
+    .arcDashAnimateTime(2000)
+    .onArcHover(hoverArc => {
+        if(hoverArc== null){
+            myGlobe.arcColor(d => {
+                return d.original_color;
+        });
+        }else{
+            myGlobe.arcColor(d => {                     
+            var split_color = hoverArc.color.substring(5, hoverArc.color.length-1).split(",");
+            //const op = !hoverArc ? split_color[3] : d === hoverArc ? 0.9 : split_color[3] / 4;
+            const op = hoverArc == d? 1:0.15
+            return `rgba(${split_color[0]}, ${split_color[1]}, ${split_color[2]}, ${op})`;
+        });
+
+        }                               
+        
+    })
+    .onArcClick(arc => {
+        midCoords = geographicMiddle(arc.startLat, arc.startLng, arc.endLat, arc.endLng)
+        // myGlobe.pointOfView({ lat: midCoords.lat, lng: midCoords.lon, altitude: 2.5}, 1000)
+        myGlobe.pointOfView({ lat: arc.endLat, lng: arc.endLng, altitude: 2.5}, 1000)
+    })
+    .arcColor('color')
+    .arcsTransitionDuration(0)
+    .arcStroke("stroke")
+    .lineHoverPrecision(0)
+
+
+    //// Polygon
+    .polygonsData(countries.features.filter(d => d.properties.ISO_A2 !== 'AQ'))
+    .polygonAltitude(0.01)
+    .polygonCapColor(feat => colorScale(getVal(feat)))
+    .polygonSideColor(() => 'rgba(0, 100, 0, 0.15)')
+    .polygonStrokeColor(() => '#111')
+    .polygonsTransitionDuration(300)
+    .polygonLabel(d => GlobaState ? '' : countryCard(d))       
+    .onPolygonHover(hoverD => {
+        myGlobe.polygonAltitude(d => d === hoverD ? 0.1 : 0.01);
+    })
+    .onPolygonClick(radiate_arcs)  
+    
+    for (const poly of myGlobe.polygonsData()) {
+        polygon_dict[poly.properties.ISO_A2] = poly;
+    }
+}
--- helpers.js
+++ helpers.js
@@ -1,88 +1,131 @@
-var convertRGBToRGBA = function(rgb, opacity=1) {     
-    /* Backward compatibility for whole number based opacity values. */
-    if (opacity > 1 && opacity <= 100) {
-        opacity = opacity / 100;   
-    }
-
-    // Check that color is not rgba already
-    var rgba = rgb
-    if(rgb != null){
-        if(rgb.indexOf('a') == -1){
-            //rgb colors are written in the from of: rgb(xxx, yyy, zzz)
-            rgba = rgb.replace(')', `, ${opacity})`).replace('rgb', 'rgba');
-        }
-    }
-    return rgba;
-};
+const nbrCountriesTable = 10;
 
 var changeCountryCard = function(card_container, country) {
-    if (country === null){
-        card_container.innerHTML = ``;
-    }
-    else{
-        card_container.innerHTML = 
-        `<div class="card">
+    card_container.innerHTML = (country === null) ? '' : countryCard(country)
+}
+
+var countryCard = function(country) {
+    return  `<div class="card">
             <img class="card-img" src="${flagEndpoint}/${country.properties.ISO_A2.toLowerCase()}.png" alt="flag" />
             <div class="card_container">
                 <span class="card-title"><b>${country.properties.ADMIN}</b></span> <br />
                 <div class="card-spacer"></div>
                 <hr />
                 <div class="card-spacer"></div>
-                <span><b>Total Trades:</b> ${country.properties.total_trades[selected_year] === -1  ? 'No Data available' : d3.format('.4s')(country.properties.total_trades[selected_year]).replace(/G/,"B USD").replace(/M/,"M USD").replace(/k/,"k USD") } - ${d3.format(".2f")(country.properties.percentage_total[selected_year])}%</span><br />
-                <span><b>Total Export:</b> ${country.properties.export_value[selected_year] === -1  ? 'No Data available' : d3.format('.4s')(country.properties.export_value[selected_year]).replace(/G/,"B USD").replace(/M/,"M USD").replace(/k/,"k USD") } - ${d3.format(",.2f")(country.properties.percentage_exports[selected_year])}%</span> <br />
-                <span><b>Total Import:</b> ${country.properties.import_value[selected_year] === -1  ? 'No Data available' : d3.format('.4s')(country.properties.import_value[selected_year]).replace(/G/,"B USD").replace(/M/,"M USD").replace(/k/,"k USD") } - ${d3.format(",.2f")(country.properties.percentage_imports[selected_year])}%</span>
+                <span><b>Total Trades:</b> ${country.properties.total_trades[selected_year] === -1  ? 'No Data available' : money_amount_fixer(country.properties.total_trades[selected_year]) } - ${d3.format(".2f")(country.properties.percentage_total[selected_year])}%</span><br />
+                <span><b>Total Export:</b> ${country.properties.export_value[selected_year] === -1  ? 'No Data available' : money_amount_fixer(country.properties.export_value[selected_year]) } - ${d3.format(",.2f")(country.properties.percentage_exports[selected_year])}%</span> <br />
+                <span><b>Total Import:</b> ${country.properties.import_value[selected_year] === -1  ? 'No Data available' : money_amount_fixer(country.properties.import_value[selected_year]) } - ${d3.format(",.2f")(country.properties.percentage_imports[selected_year])}%</span>
     
                 <div class="card-spacer"></div>
                 <hr />
                 <div class="card-spacer"></div>
-                <span><b>Largest exports:</b> ${country.properties.trade_value_exported[selected_year] === -1  ? 'No Data available' : d3.format('.4s')(country.properties.trade_value_exported[selected_year]).replace(/G/,"B USD").replace(/M/,"M USD").replace(/k/,"k USD")} to ${country.properties.highest_export_to[selected_year]} </span><br />
-                <span><b>Largest Imports:</b> ${country.properties.trade_value_imported[selected_year] === -1  ? 'No Data available' : d3.format('.4s')(country.properties.trade_value_imported[selected_year]).replace(/G/,"B USD").replace(/M/,"M USD").replace(/k/,"k USD")} from ${country.properties.highest_import_from[selected_year]} </span><br />
+                <span><b>Largest exports:</b> ${country.properties.trade_value_exported[selected_year] === -1  ? 'No Data available' : money_amount_fixer(country.properties.trade_value_exported[selected_year])} to ${country.properties.highest_export_to[selected_year]} </span><br />
+                <span><b>Largest Imports:</b> ${country.properties.trade_value_imported[selected_year] === -1  ? 'No Data available' : money_amount_fixer(country.properties.trade_value_imported[selected_year])} from ${country.properties.highest_import_from[selected_year]} </span><br />
     
                 <!-- <div class="card-spacer"></div>
                 <hr />
                 <div class="card-spacer"></div>
                 <span><b>Largest Export:</b> ${country.properties.Export_trade_value_usd  === -1 ? 'No Data available' : country.properties.Export_commodity}</span><br />
-                <span><b>Value largest Export: </b>${country.properties.Export_trade_value_usd  === -1 ? 'No Data available' : d3.format('.4s')(country.properties.Export_trade_value_usd).replace(/G/,"B USD").replace(/M/,"M USD").replace(/k/,"k USD") } </span><br /><br />             
+                <span><b>Value largest Export: </b>${country.properties.Export_trade_value_usd  === -1 ? 'No Data available' : money_amount_fixer(country.properties.Export_trade_value_usd)} </span><br /><br />             
                 <span><b>Largest Import:</b> ${country.properties.Import_trade_value_usd  === -1  ? 'No Data available' : country.properties.Import_commodity}</span><br />
-                <span><b>Value largest Import: </b>${country.properties.Import_trade_value_usd  === -1  ? 'No Data available' : d3.format('.4s')(country.properties.Import_trade_value_usd).replace(/G/,"B USD").replace(/M/,"M USD").replace(/k/,"k USD") } </span>             
+                <span><b>Value largest Import: </b>${country.properties.Import_trade_value_usd  === -1  ? 'No Data available' : money_amount_fixer(country.properties.Import_trade_value_usd)} </span>             
                 </div> -->
             </div>
-        </div>`;
-    }
+            </div>`;
 }
 
+var changeCountryTable = function(table, tableBody, countries, tradeType) {
+    tradeTypeHead = tradeType=="import_value" ? "Imports from (USD)" : "Exports to (USD)";
+    table.tHead.innerHTML = `<tr><th colspan = "2" >${tradeTypeHead}</th></tr>`;
+    table.tHead.addEventListener("click", function(){
+        //tradeType = document.getElementById("trade").value
+        document.getElementById("trade").value = tradeType=="import_value" ? "export_value" : "import_value"
+        onTradeChange()
+    })
+    countries = countries.slice(0, nbrCountriesTable);
 
+    var idx = 0;
+    const rowNumber = tableBody.rows.length;
+    countries.forEach(country => {
+        rowNumber == 0 ? addRowTable(tableBody, country, idx) : updateRowTable(tableBody, idx, country);
+        idx++;
+    })
+    table.style.visibility='visible'
+}
 
+function addRowTable(tableBody, country, rowNumber){
+    var newRow = tableBody.insertRow(rowNumber);
+    var nameCell = newRow.insertCell(0);
+    nameCell.addEventListener("click",function(){
+        myGlobe.pointOfView(country[2], 1000)
+    });
+    var valueCell = newRow.insertCell(1);
+    var countryName = document.createTextNode(`${rowNumber+1}. ${country[0]}`);
+    var value = document.createTextNode(country[1]);
+    nameCell.appendChild(countryName);
+    valueCell.appendChild(value);
+}
 
-var changeCountryTable = function(table_container, countries, export_import) {
-    if (country === null){
-        table_container.innerHTML = ``;
-    }
+function updateRowTable(tableBody, rowNumber, country){
+    tableBody.rows[rowNumber].cells[0].innerText = `${rowNumber+1}. ${country[0]}`;
+    tableBody.rows[rowNumber].cells[0].addEventListener("click",function(){
+        myGlobe.pointOfView(country[2], 1000)
+    });
+    tableBody.rows[rowNumber].cells[1].innerText = country[1];
+}
+
+var money_amount_fixer = function(amount){
+    return d3.format('.4s')(amount).replace(/G/,"B USD").replace(/M/,"M USD").replace(/k/,"k USD")
+}  
+
+var PolygonColorChanger = function(d,polygon,arcArray){
+    var targets_A2 = arcArray.map(x => x[3]);
+    var target_Name = arcArray.map(x => x[4]);
+    if (d === polygon){
+        return 'steelblue'
+    }else if(targets_A2.includes(d.properties.ISO_A2) || target_Name.includes(d.properties.ADMIN)){
+        return 'lightsalmon'
+    }                    
     else{
-        table_container.innerHTML = 
-        `<table border = "1" cellpadding = "5" cellspacing = "5">
-                <tr>
-                    <th colspan = "2">${export_import}</th>
-                 </tr>
-                 <tr>
-                    <th>Name</th>
-                    <th>Salary</th>
-                 </tr>
-                 <tr>
-                    <td>Ramesh Raman</td>
-                    <td>5000</td>
-                 </tr>
-                 <tr>
-                    <td>Shabbir Hussein</td>
-                    <td>7000</td>
-                 </tr>
-        </table>`;
+        return 'grey'
     }
-    return ``;
+}
+
+
+var load_trade_data = function(prods){
+    prods.forEach(prod => {
+        fetch('./dataset/trade_data_' + prod + '.json')
+            .then(x => x.json()).then(trades => {products_dict[prod] = trades})
+            .then(_ => {if (prod==='all'){current_trades = products_dict[prod];}});
+    });
+    onProductChange('all')
 }
 
 
 
+var geographicMiddle = function(lat1, lng1, lat2, lng2) {
+	
+    //-- Longitude difference
+    var dLng = degToRad(lng2 - lng1);
 
+    //-- Convert to radians
+    lat1 = degToRad(lat1);
+    lat2 = degToRad(lat2);
+    lng1 = degToRad(lng1);
 
+    var bX = Math.cos(lat2) * Math.cos(dLng);
+    var bY = Math.cos(lat2) * Math.sin(dLng);
+    var lat3 = Math.atan2(Math.sin(lat1) + Math.sin(lat2), Math.sqrt((Math.cos(lat1) + bX) * (Math.cos(lat1) + bX) + bY * bY));
+    var lng3 = lng1 + Math.atan2(bY, Math.cos(lat1) + bX);
 
+    //-- Return result
+    return {'lat':radToDeg(lat3), 'lon':radToDeg(lng3)};
+}
+
+var degToRad = function(deg) {
+    return deg * (Math.PI / 180.0);
+}
+
+var radToDeg = function(rad) {
+    return rad * (180.0 / Math.PI);
+}
