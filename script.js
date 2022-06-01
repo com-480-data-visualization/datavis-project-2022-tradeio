9aa6528e90cf1d5e3fc185fc8c062bb1ed57a49e
Ziwei-Liu3 <ziwei.liu@epfl.ch>
2022-06-01 18:30:58 +0200

    play button & slider  fixed and start from 1962 when hitting play

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 // Slider
 const slider = document.querySelector('.slider');
 // Play button
@@ -7,45 +6,66 @@ const playButton = document.querySelector('.play-button');
 var output = document.getElementById("demo");
 // Slider date
 const sliderDate = document.querySelector('.slider-date');
-// playButton.disabled = false;
+playButton.disabled = false;
 
-// slider.oninput = function() {
-//   output.innerHTML = slider.value;
-//   slider.click();
-// }
-// onYearChange(slider.value);
-output.innerHTML = slider.value;
+slider.oninput = function() {
+  output.innerHTML = slider.value;
+  onYearChange(slider.value);
+  slider.click();
+}
 
+function yearLabel(){
+  output.innerHTML = slider.value;
+  onYearChange(slider.value);
+}
 
+/*const
+  range = document.getElementById('years'),
+  rangeV = document.getElementById('rangeV'),
+  setValue = ()=>{
+    const
+      newValue = Number( (range.value - range.min) * 94 / (range.max - range.min) ),
+      newPosition = 55 - (newValue * 0.2);
+      newPositionTop = -34.5;
+    rangeV.innerHTML = `<span>${range.value}</span>`;
+    rangeV.style.left = `calc(${newValue}% + (${newPosition}px))`;
+    rangeV.style.top = `${newPositionTop}px`;
+  };*/
+// document.addEventListener("DOMContentLoaded", setValue);
+// range.addEventListener('input', setValue);
 
 // when play button is clicked on 
 playButton.addEventListener('click', () => {
     if (playButton.innerText == 'Play') {
+      slider.value = 1962;
       playButton.innerText = 'Pause';
     } else {
       playButton.innerText = 'Play';
       clearInterval(interval);
-      // slider.click();
+      slider.click();
       return;
     }
   // Check if slider position is max
   if (+slider.value === 2019) {
     slider.value = 2019;
   }
-  // sliderDate.innerHTML = slider.value;
   output.innerHTML = slider.value;
+  slider.click();
+  onYearChange(slider.value);
 
   interval = setInterval(() => {
     slider.value++;
-    // sliderDate.innerHTML = slider.value;
     output.innerHTML = slider.value;
-    selected_year = selected_year + 1;
-    onYearChange(slider.value);
     slider.click();
-    // onYearChange(selected_year);
+    onYearChange(slider.value);
+    // setValue();
+    // rangeV.innerHTML = `<span>${range.value}</span>`;
+    slider.click();
     if (+slider.value === 2019) {
       playButton.innerHTML = 'Play';
       slider.value = 2019;
+      onYearChange(slider.value);
+      slider.click();
       clearInterval(interval);
     }
   }, 200);
@@ -54,8 +74,9 @@ if ('oninput' in slider) {
     slider.addEventListener(
     'input',
     function () {
-        selected_year = selected_year + 1;
         onYearChange(slider.value);
+        // setValue();
+        // rangeV.innerHTML = `<span>${range.value}</span>`;
         slider.click();
     },
     false
@@ -64,3 +85,5 @@ if ('oninput' in slider) {
 }
 
 output.innerHTML = slider.value;
+slider.click();
+onYearChange(slider.value);
